90c3f4cbb4f5fd71ce0f13657c35f95dd0af7a92
fukawa <ta1fukawa@gmail.com>
2022-06-06 21:28:26 +0900

    Update

--- code/modules/audio.py
+++ code/modules/audio.py
@@ -1,140 +1,116 @@
+import matplotlib.pyplot as plt
 import sys
 
-import matplotlib; matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import librosa
 import numpy as np
 import soundfile as sf
+from scipy.io import wavfile
+import wavenet_vocoder
 import torch
-import torchaudio
+from tqdm import tqdm
 
 sys.path.append('code')
 
 from modules import global_value as g
 
 
-def save_spec_fig(c, t, r, q):
-    c = c.squeeze(0).detach().cpu().numpy()
-    t = t.squeeze(0).detach().cpu().numpy()
-    r = r.squeeze(0).detach().cpu().numpy()
-    q = q.squeeze(0).detach().cpu().numpy()
+def load_wav(path):
+    sr, wave = wavfile.read(path)
 
-    (g.work_dir / 'spec').mkdir(parents=True)
+    if wave.dtype == np.int16:
+        wave = wave / 2**15
+    elif wave.dtype == np.int32:
+        wave = wave / 2**31
+    elif wave.dtype == np.float32:
+        pass
+    else:
+        raise ValueError('Unsupported wav format')
 
-    plt.figure(figsize=(10, 6))
-    plt.imshow(c.T[::-1], cmap='magma', vmin=g.spec_fig['min_db'], vmax=g.spec_fig['max_db'])
-    plt.savefig(str(g.work_dir / 'spec' / 'source_content.png'))
+    if sr != g.sample_rate:
+        wave = librosa.resample(wave, sr, g.sample_rate)
 
-    plt.figure(figsize=(10, 6))
-    plt.imshow(t.T[::-1], cmap='magma', vmin=g.spec_fig['min_db'], vmax=g.spec_fig['max_db'])
-    plt.savefig(str(g.work_dir / 'spec' / 'target_content.png'))
+    wave = np.clip(wave, -1., 1.)
+    wave = _low_cut_filter(wave, g.highpass_cutoff)
 
-    plt.figure(figsize=(10, 6))
-    plt.imshow(r.T[::-1], cmap='magma', vmin=g.spec_fig['min_db'], vmax=g.spec_fig['max_db'])
-    plt.savefig(str(g.work_dir / 'spec' / 'predict_before.png'))
+    wave = np.pad(wave, (0, g.fft_size), mode='constant')
+    mel  = wave2mel(wave)
 
-    plt.figure(figsize=(10, 6))
-    plt.imshow(q.T[::-1], cmap='magma', vmin=g.spec_fig['min_db'], vmax=g.spec_fig['max_db'])
-    plt.savefig(str(g.work_dir / 'spec' / 'predict_after.png'))
+    wave_len = mel.shape[0] * g.hop_size
+    wave = wave[:wave_len]
 
-
-def save_mel_wave(c, t, r, q, angle):
-    source_wave  = mel2wave(c[0], angle[0], ver=2, **g.mel_spec)
-    target_wave  = mel2wave(t[0], angle[0], ver=2, **g.mel_spec)
-    predict_wave = mel2wave(q[0], angle[0], ver=2, **g.mel_spec)
-
-    source_wave_  = source_wave.squeeze(0).detach().cpu().numpy()
-    target_wave_  = target_wave.squeeze(0).detach().cpu().numpy()
-    predict_wave_ = predict_wave.squeeze(0).detach().cpu().numpy()
-
-    (g.work_dir / 'wav').mkdir(parents=True)
-
-    sf.write(str(g.work_dir / 'wav' / f'source.wav'),  source_wave_,  g.mel_spec['sample_rate'])
-    sf.write(str(g.work_dir / 'wav' / f'target.wav'),  target_wave_,  g.mel_spec['sample_rate'])
-    sf.write(str(g.work_dir / 'wav' / f'predict.wav'), predict_wave_, g.mel_spec['sample_rate'])
+    return wave, mel
 
 
-def wave2mel(wave, sample_rate, fft_window_ms, fft_hop_ms, n_fft, f_min, n_mels, ref_db, dc_db):
-    spec = torchaudio.transforms.Spectrogram(
-        n_fft=n_fft,
-        win_length=int(sample_rate * fft_window_ms / 1000),
-        hop_length=int(sample_rate * fft_hop_ms / 1000),
-        power=None,
-    ).to(g.device)(wave)
-    radii = torch.abs(spec)
-    angle = torch.angle(spec)
-
-    mel = torchaudio.transforms.MelScale(
-        n_mels=n_mels,
-        sample_rate=sample_rate,
-        f_min=f_min,
-        n_stft=n_fft // 2 + 1,
-    ).to(g.device)(radii)
-
-    mel = 20 * torch.log10(torch.clamp(mel, min=1e-9))
-    mel = (mel - ref_db) / (dc_db - ref_db)
-
-    mel   = mel.squeeze(0).T
-    angle = angle.squeeze(0).T
-    return mel, angle
+def save_wav(wave, path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    sf.write(path, wave, g.sample_rate)
 
 
-def mel2wave(mel, angle, ver=1, **kwargs):
-    if ver == 1:
-        wave = mel2wave_v1(mel, angle, **kwargs)
-    elif ver == 2:
-        wave = mel2wave_v2(mel, **kwargs)
-    return wave
+def _low_cut_filter(wave, cutoff):
+    nyquist = g.sample_rate // 2
+    norm_cutoff = cutoff / nyquist
 
+    from scipy.signal import firwin, lfilter
 
-def mel2wave_v1(mel, angle, sample_rate, fft_window_ms, fft_hop_ms, n_fft, f_min, n_mels, ref_db, dc_db):
-    mel   = mel.T.unsqueeze(0)
-    angle = angle.T.unsqueeze(0)
-
-    mel = mel * (dc_db - ref_db) + ref_db
-    mel = 10**(mel / 20)
-
-    radii = torchaudio.transforms.InverseMelScale(
-        n_stft=n_fft // 2 + 1,
-        n_mels=n_mels,
-        sample_rate=sample_rate,
-        f_min=f_min,
-    ).to(g.device)(mel)
-
-    spec = radii * torch.exp(1j * angle[:, :, :radii.size(2)])
-
-    wave = torchaudio.transforms.InverseSpectrogram(
-        n_fft=n_fft,
-        win_length=int(sample_rate * fft_window_ms / 1000),
-        hop_length=int(sample_rate * fft_hop_ms / 1000),
-    ).to(g.device)(spec)
+    fil = firwin(255, norm_cutoff, pass_zero=False)
+    wave = lfilter(fil, 1, wave)
 
     return wave
 
 
-def mel2wave_v2(mel, sample_rate, fft_window_ms, fft_hop_ms, n_fft, f_min, n_mels, ref_db, dc_db):
-    if not hasattr(g, 'waveglow'):
-        # g.waveglow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp32')
-        # g.waveglow = g.waveglow.remove_weightnorm(g.waveglow)
-        # g.waveglow = g.waveglow.to('cuda')
-        # g.waveglow.eval()
-        g.waveglow = torchaudio.pipelines.TACOTRON2_WAVERNN_PHONE_LJSPEECH.get_vocoder().to(g.device)
+def load_mel(path):
+    mel = np.load(path)
 
-    mel = mel.T.unsqueeze(0)
+    return mel
+
+
+def save_mel(mel, path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    np.save(path, mel)
+
+
+def save_mel_img(mel, path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    plt.figure(figsize=(10, 6))
+    plt.subplots_adjust(left=0.1, right=0.95, bottom=0.1, top=0.95)
+    plt.imshow(mel.T, cmap='magma', aspect='auto', origin='lower')
+    plt.savefig(path)
+    plt.close()
+
+
+g._mel_basis = None
+def wave2mel(wave):
+    if g._mel_basis is None:
+        g._mel_basis = librosa.filters.mel(g.sample_rate, g.fft_size, g.num_mels, g.fmin, g.fmax)
+
+    spec = librosa.stft(wave, n_fft=g.fft_size, hop_length=g.hop_size, win_length=g.win_length, window=g.window, pad_mode='constant')
+    mel = np.dot(g._mel_basis, np.abs(spec))
+    mel = np.log10(np.maximum(mel, 1e-10))
+
+    return mel.astype(np.float32).T
+
+
+g._wavenet_model = None
+def mel2wave(mel):
+    if g._wavenet_model is None:
+        g._wavenet_model = wavenet_vocoder.WaveNet(**g.wavenet_model, scalar_input=True).to(g.device)
+        g._wavenet_model.eval()
+        g._wavenet_model.make_generation_fast_()
+
+    wave_length = mel.shape[0] * g.hop_size
+
+    initial_input = torch.zeros(1, 1, 1).fill_(0.0).to(g.device)
+    mel = torch.from_numpy(mel.T).unsqueeze(0).to(g.device)
 
     with torch.no_grad():
-        # wave = g.waveglow.infer(mel)
-        wave, _ = g.waveglow(mel, mel.shape)
+        wave = g._wavenet_model.incremental_forward(
+            initial_input, c=mel, g=None, T=wave_length, tqdm=tqdm, softmax=True, quantize=True,
+            log_scale_min=g.log_scale_min,
+        )
+    
+    wave = wave.squeeze(0).cpu().numpy()
 
     return wave
-
-
-def mel2embed(mels, encoder, seg_len):
-    mels = torch.stack([
-        mel[:seg_len] for mel in mels if len(mel) >= seg_len
-    ], dim=0).to(g.device)
-
-    with torch.no_grad():
-        embeds = encoder(mels)
-
-    embed = torch.mean(embeds, dim=0)
-    return embed
