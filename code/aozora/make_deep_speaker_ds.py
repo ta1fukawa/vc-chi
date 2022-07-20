0952d12ba3305d4148aa2be99c68fd0350472e00
fukawa <ta1fukawa@gmail.com>
2022-07-21 02:25:53 +0900

    Update

--- code/aozora/make_deep_speaker_ds.py
+++ code/aozora/make_deep_speaker_ds.py
@@ -2,22 +2,28 @@ import pathlib
 import sys
 
 import numpy as np
+import multiprocessing
 
 sys.path.append('code/deep-speaker/')
 
 from audio import read_mfcc
 from constants import SAMPLE_RATE
 
-wav_dir = pathlib.Path('./dataset/jvs_ver1/wav_all')
+wav_dir = pathlib.Path('./dataset/aozora/wav_seg')
 dest_dir = pathlib.Path('~/.deep-speaker-wd/my-training/audio-fbanks').expanduser()
 dest_dir.mkdir(parents=True, exist_ok=True)
 
-for speaker in sorted(wav_dir.iterdir()):
-    if not speaker.is_dir():
-        continue
+def process(speaker):
+    speeches = sorted(speaker.iterdir())
+    if len(speeches) <= 10:
+        return
 
-    for speech in sorted(speaker.iterdir()):
+    for speech in speeches:
         mfcc = read_mfcc(speech, SAMPLE_RATE)  # MFCCと書いてあるが、実際はメルスペクトログラムに変換される(NMELS=64)
-        speech_name = speech.stem.split('_')[1]
-        dest_path = dest_dir / f'{speaker.name}_{speech_name}.npy'
+        speech_name = speech.stem.replace('_', '-')
+        dest_path = dest_dir / f'aozora{speaker.name}_{speech_name}.npy'
+        print(dest_path)
         np.save(dest_path, mfcc)
+
+pool = multiprocessing.Pool(processes=32)
+pool.map(process, sorted(wav_dir.iterdir()))
